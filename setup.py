5cce7f0c50593129044d98fa6f182432b46c4960
Stefan Eng <stefaneng13@gmail.com>
2014-09-28 10:07:55 -0700

    Add skeleton python project and travis yaml

---
+++ setup.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python
+
+from distutils.core import setup
+
+setup(name='ExampleTravisCI',
+      version='1.0',
+      description='Example Travis CI Script',
+      author='Stefan Eng',
+      author_email='stefaneng13@gmail.com',
+      url='https://github.com/stefaneng/ExampleTravisCI'
+     )
---
+++ tests/example_test.py
@@ -0,0 +1,3 @@
+def test():
+    "Example test"
+    assert 1 == 1
